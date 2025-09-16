c9d4ccc6623051c8bd3ff11d120e88fd9351b1d0
vlvtvdz <kabishevivan@gmail.com>
2025-09-16 21:31:10 +0300

    jbvjbsj

--- 4.cpp
+++ 4.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
-int main()
-{
-    int k,a,p1,p2;
+
+int main() {
+    long long k, a, p1;
     std::cout << "Введите k: ";
-    if (!(std::cin >> k)) {
-        std::cerr << "Ошибка: введено не число!" << std::endl;
-        return 1; 
+    if (!(std::cin >> k) || k < 0) {
+        std::cerr << "Ошибка: введено другое число!" ;
+        return 1;
     }
-    while (a<=k) {
-        if (k % 2 == 0){
-            p1 = 2;
-            a = 4;
+    if (k % 2 == 0) {
+        a = 2;
+        p1 = a;
+        while (a <= k){
+            p1 *= a;
+            a += 2; 
+        }     
+        std::cout << "Двойной равен: " << p1; 
+    }
+    else {
+        a = 1;
+        p1 = a;
+        while (a <= k){
             p1 *= a;
             a += 2;
-        }
-        else {
-            p2 = 1;
-            a = 3;
-            p2 *= a;
-            a += 2;
-        }
+        }    
+        std::cout << "Двойной равен: " << p1;
     }
-    std::cout << "Произведение равно: " <<p1;
-    std::cout << "Произведение равно: " <<p2;
-
 }
