17bf100eec403bac368d4494fa06c6da5eb5557b
vlvtvdz <kabishevivan@gmail.com>
2025-09-17 11:36:06 +0300

    lab_2_7

--- semester_2/lab_2_7.cpp
+++ semester_2/lab_2_7.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
-int main () {
-    long long n, a, m, b,c;
-    std::cout << "Введите k: ";
-    if (!(std::cin >> n >> m) || n,m < 1) {
-        std::cerr << "Ошибка: введите другое число!" ;
+using namespace std;
+
+int main() {
+    long long n;
+    cout << "Введите n: ";
+    if (!(cin >> n) || n < 1) {
+        cerr << "Ошибка: введите натуральное число!" << endl;
         return 1;
     }
-    if (m >> n){
-        
+
+    for (long long a = 1; a <= n; a++) {
+        for (long long b = a; b <= n; b++) {
+            for (long long c = b; c <= n; c++) {
+                if (a * a + b * b == c * c) {
+                    cout << "Пифагорова тройка: "
+                         << a << " " << b << " " << c << endl;
+                }
+            }
+        }
     }
+
+    return 0;
 }
