8d185a421ae8114f39e3d550c0fb8db97434613e
vlvtvdz <kabishevivan@gmail.com>
2025-09-17 10:47:59 +0300

    lab_2_7

--- semester_1/2.cpp
+++ semester_1/2.cpp
@@ -2,7 +2,6 @@
 int main ()
 {
     int a, n, c, d;
-    long long k, a, p1;
     std::cout << "Введите k: ";
     if (!(std::cin >> n) || n < 2) {
         std::cerr << "Ошибка: введите другое число!" ;
---
+++ semester_2/lab_2_7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main () {
+    long long n, a, m, b,c;
+    std::cout << "Введите k: ";
+    if (!(std::cin >> n >> m) || n,m < 1) {
+        std::cerr << "Ошибка: введите другое число!" ;
+        return 1;
+    }
+    if (m >> n){
+        
+    }
+}
